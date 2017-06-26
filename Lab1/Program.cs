27410081c1e3910ddecec81a2ec56eed43f86bec
JJ Ryan <jryan@motor.com>
2017-06-26 15:54:41 -0400

    FIxed invalid Y or N problem

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,10 +4,26 @@ namespace Lab1
 {
     class Program
     {
+        static string ynInput()
+        {
+            string input = "";
+            bool invalid = true;
+            while (invalid) {
+                Console.WriteLine("Continue? (y/n): ");
+                input = Console.ReadLine();
+                if ((input == "y") || (input == "n"))
+                {
+                    invalid = false;
+                }
+            }
+            return input;
+        }
+
         static void Main(string[] args)
         {
             double length = 0;
             double width = 0;
+            double height = 0;
             string yesNo = "y";
             bool cont = true;
             while (cont){
@@ -15,12 +31,15 @@ namespace Lab1
                 length = double.Parse(Console.ReadLine());
                 Console.Write("Enter Width: ");
                 width = double.Parse(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                height = double.Parse(Console.ReadLine());
                 double area = length * width;
                 double perimeter = 2 * (length + width);
+                double volume = length * width * height;
                 Console.WriteLine("Area: {0}", area);
                 Console.WriteLine("Perimeter: {0}", perimeter);
-                Console.WriteLine("Continue? (y/n): ");
-                yesNo = Console.ReadLine();
+                Console.WriteLine("Volume: {0}", volume);
+                yesNo = ynInput();
                 if (yesNo == "n")
                 {
                     cont = false;
