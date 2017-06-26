5ca43bff12b38be9115deaf824d6952eebac5e6c
JJ Ryan <jryan@motor.com>
2017-06-26 14:53:27 -0400

    here goes nothing

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab1
 {
@@ -10,6 +6,26 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            double length = 0;
+            double width = 0;
+            string yesNo = "y";
+            bool cont = true;
+            while (cont){
+                Console.Write("Enter Length: ");
+                length = double.Parse(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                width = double.Parse(Console.ReadLine());
+                double area = length * width;
+                double perimeter = 2 * (length + width);
+                Console.WriteLine("Area: {0}", area);
+                Console.WriteLine("Perimeter: {0}", perimeter);
+                Console.WriteLine("Continue? (y/n): ");
+                yesNo = Console.ReadLine();
+                if (yesNo == "n")
+                {
+                    cont = false;
+                }
+            }
         }
     }
 }
